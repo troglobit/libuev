9a1f7aaa39719a6ec5998c2bb5a8c78723de27f9
ant0nT <30862396+ant0nT@users.noreply.github.com>
2024-03-16 23:47:15 +0300

    clean cmake code

--- toolchains/armhf/armhf_toolchain.cmake
+++ toolchains/armhf/armhf_toolchain.cmake
@@ -1,5 +1,3 @@
 set(CMAKE_C_COMPILER /usr/bin/arm-linux-gnueabihf-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/arm-linux-gnueabihf-g++)
 set(CMAKE_LINKER /usr/bin/arm-linux-gnueabihf-ld)
-# set(CMAKE_BUILD_TYPE Release)
-# include("../../src/CMakeLists.txt")
